53eea9ecccef6cea7e43997db2c6cd0120b1f083
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-05-02 22:17:01 +0200

    Removed comment

--- background/background.js
+++ background/background.js
@@ -1,8 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
 
-    //Populate synced storage to avoid null errors in future
-    //chrome.storage.sync.set({toggleState:0});
-
     //Make extension only run on meet.google.com
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([
