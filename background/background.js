4bc5f1646a594efd7ee39386a58abba68ea2b11e
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-05-02 15:52:08 +0200

    Initial commit.

---
+++ background/background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {conditions:[new chrome.declarativeContent.PageStateMatcher({pageUrl:{hostEquals:'meet.google.com'}})], actions: [new chrome.declarativeContent.ShowPageAction()]}
+        ]);
+    });
+
+});
