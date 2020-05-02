b0b6a8f893c64b9889676c597cf96f056ecab413
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-05-02 22:16:09 +0200

    Added base functionality

--- background/background.js
+++ background/background.js
@@ -1,5 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
 
+    //Populate synced storage to avoid null errors in future
+    //chrome.storage.sync.set({toggleState:0});
+
+    //Make extension only run on meet.google.com
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([
             {conditions:[new chrome.declarativeContent.PageStateMatcher({pageUrl:{hostEquals:'meet.google.com'}})], actions: [new chrome.declarativeContent.ShowPageAction()]}
---
+++ scripts/index.js
@@ -0,0 +1,75 @@
+window.addEventListener("load", () => {
+    chrome.storage.sync.get("toggleState", (response) => {currentState(response.toggleState)});
+    chrome.storage.onChanged.addListener((changes) => {
+        currentState(changes.toggleState.newValue);
+    });
+});
+
+currentState = (bool) => {
+    let spanElement = document.getElementsByTagName("span")[0];
+    if(bool) {
+        spanElement.innerText = "active";
+        spanElement.style.color = "#34e234";
+    }
+    else {
+        spanElement.innerText = "inactive";
+        spanElement.style.color = "red";
+    }
+};
+
+isInACall = () => {
+    let isInACallBool;
+    let appEl = document.getElementById("app");
+    if(appEl == null || appEl.children.length == 0) {
+        isInACallBool = false;
+    }
+    else {
+        isInACallBool = true;
+    }
+    return isInACallBool;
+};
+
+addScript = () => {
+    let hasTooltipEl = document.querySelectorAll("[data-tooltip]");
+    let tooltipElIndex;
+    for(let i = 0; i < hasTooltipEl.length; i++) {
+        if(hasTooltipEl[i].getAttribute("data-tooltip").includes("ctrl + d")){tooltipElIndex = i;}
+    }
+
+    window.addEventListener("blur", blur = () => {
+        if(hasTooltipEl[tooltipElIndex].getAttribute("data-is-muted") == "false") {
+            hasTooltipEl[tooltipElIndex].click();
+        }
+    });
+    window.addEventListener("focus", focus = () =>{
+        if(hasTooltipEl[tooltipElIndex].getAttribute("data-is-muted") == "true") {
+            hasTooltipEl[tooltipElIndex].click();
+        }
+    });
+};
+
+removeScript = () => {
+    window.removeEventListener("blur", blur);
+    window.removeEventListener("focus", focus);
+};
+
+document.getElementsByTagName("button")[0].addEventListener("click", ()=> {
+    chrome.tabs.executeScript({code:'(' + isInACall + ')();'}, (inACall) =>{
+        if(inACall[0]) {
+            chrome.storage.sync.get("toggleState", (boolean) => {
+                console.log(boolean.toggleState);
+                if (boolean.toggleState) {
+                    chrome.tabs.executeScript({code: '(' + removeScript + ')();'});
+                    chrome.storage.sync.set({toggleState: 0});
+                } else {
+                    chrome.tabs.executeScript({code: '(' + addScript + ')();'});
+                    chrome.storage.sync.set({toggleState: 1});
+                }
+            });
+        }
+        else {
+            document.getElementsByClassName("inACall")[0].innerText = "Enter a call first";
+        }
+
+    });
+});
